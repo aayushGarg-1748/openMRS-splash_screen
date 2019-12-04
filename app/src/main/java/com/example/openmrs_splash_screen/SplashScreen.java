42a39a8fd978e4040c55cdddca8234b10b03bb65
aayushGarg-1748 <argtech.1234@gmail.com>
2019-12-04 21:31:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/openmrs_splash_screen/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.openmrs_splash_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+    }
+}
