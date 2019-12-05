b922e01194f0dd55a58935b4dfd1a88c9316c962
aayushGarg-1748 <argtech.1234@gmail.com>
2019-12-05 17:07:57 +0530

    added changes

--- app/src/main/java/com/example/openmrs_splash_screen/SplashScreen.java
+++ app/src/main/java/com/example/openmrs_splash_screen/SplashScreen.java
@@ -2,7 +2,12 @@ package com.example.openmrs_splash_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,6 +15,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+        ImageView imageView = findViewById(R.id.image1);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.splash_animation);
+
+        imageView.startAnimation(animation);
 
 
     }
